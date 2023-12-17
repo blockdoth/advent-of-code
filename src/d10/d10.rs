824ecd2310df46e69b5826856f4422eb3d5ebff3
Pepijn van Egmond <P.O.vanEgmond@student.tudelft.nl>
2023-12-17 02:10:26 +0100

    d10 p2 dead end

--- src/d10/d10.rs
+++ src/d10/d10.rs
@@ -1,10 +1,10 @@
 pub mod d10 {
-    use std::collections::{HashMap, HashSet, VecDeque};
+    use std::collections::{HashMap, VecDeque};
 
 
     pub(crate) fn d10(file: Vec<String>) -> (i32, i32) {
         let mut result1 = 0;
-        let result2 = 0;
+        let mut result2 = 0;
 
 /*      The pipes are arranged in a two-dimensional grid of tiles:
 
@@ -87,17 +87,15 @@ pub mod d10 {
 
         // building the value grid with padding
         let mut value_grid = Vec::with_capacity(height);
-        for _ in 0..width + 2{
+        for _ in 0..height + 2{
             value_grid.push(vec![0; width + 2]);
         }
 
         // traversing breadth first
         let mut char_q:VecDeque<(usize,usize)> = VecDeque::new();
-        let mut explored: HashSet<(usize,usize)> = HashSet::new();
-
         char_q.push_back(start_coordinates);
-        explored.insert(start_coordinates);
-        value_grid[start_coordinates.1][start_coordinates.0] = 0;
+
+        value_grid[start_coordinates.1][start_coordinates.0] = 1;
         let mut first_iter = true;
         while !char_q.is_empty(){
             let current_coordinates = char_q.pop_front().expect("queue error");
@@ -122,19 +120,16 @@ pub mod d10 {
                             }
                         }
                         //println!("{}", char_to_check);
-                        let next_pipe = (x_cord,y_cord);
-                        if !explored.contains(&next_pipe){
-                            char_q.push_back(next_pipe);
-                            explored.insert(next_pipe);
+                        if value_grid[y_cord][x_cord] == 0 {
+                            char_q.push_back((x_cord,y_cord));
                             let current_distance = value_grid[current_coordinates.1][current_coordinates.0] + 1;
                             if current_distance > result1 {
                                 result1 = current_distance;
                             }
                             //println!("found {} at {}{}",get_char_at_coords(char_grid.clone(),x_cord,y_cord),x_cord,y_cord);
-                            value_grid[next_pipe.1][next_pipe.0] = current_distance;
+                            value_grid[y_cord][x_cord] = current_distance;
                         }
                     }
-
                 }
             }
             first_iter = false;
@@ -143,7 +138,38 @@ pub mod d10 {
         }
         print_string_grid_with_updates(&char_grid, &value_grid,result1);
 
-        (result1, result2)
+        let mut value_q:VecDeque<(i32,i32)> = VecDeque::new();
+        let flood_start_location = (0,0);
+        value_q.push_back(flood_start_location);
+        value_grid[0][0] = -1;
+        while !value_q.is_empty(){
+            let current_location = value_q.pop_front().expect("");
+            for y_offset in -1..2 {
+                for x_offset in -1..2 {
+                    let x_cord = current_location.0 + x_offset;
+                    let y_cord = current_location.1 + y_offset;
+                    if x_cord >= 0 && x_cord < width as i32 + 2 && y_cord >= 0 && y_cord < height as i32 + 2 {
+                        if value_grid[y_cord as usize][x_cord as usize] == 0 {
+                            value_grid[y_cord as usize][x_cord as usize] = -1;
+                            value_q.push_back((x_cord,y_cord));
+                        }
+                    }
+                }
+            }
+        }
+
+        for (y,line) in value_grid.iter().enumerate() {
+            for (x,value) in line.iter().enumerate() {
+                if *value == 0  && char_grid[y][x] == '.' {
+                    result2 += 1;
+                }
+            }
+        }
+        print_int_grid(&value_grid);
+
+
+
+        (result1 - 1, result2)
     }
     fn print_string_grid_with_updates(string_grid: &Vec<Vec<char>>, value_grid: &Vec<Vec<i32>>, highest_value: i32) {
         for (y,line) in string_grid.iter().enumerate() {
@@ -181,10 +207,17 @@ pub mod d10 {
         }
         result
     }
-    fn print_int_grid(int_grid: Vec<Vec<i32>>) {
+    fn print_int_grid(int_grid: &Vec<Vec<i32>>) {
         for line in int_grid{
             for x in line {
-                print!(" {}", x);
+                if *x == -1{
+                    print!("\x1b[31m{}\x1b[0m", x);
+                } else if *x > 0{
+                    print!("\x1b[32m {}\x1b[0m", 1);
+
+                }else{
+                    print!(" {}", x);
+                }
             }
             print!("\n")
         }
